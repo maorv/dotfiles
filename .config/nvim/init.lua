542e070a1beb191e83ab06a14aef8405b80eaa88
Maor Vanmak <maor@lightbitslabs.com>
2022-10-06 07:25:09 +0300

    Neovim: initial import of neovim config.
    Signed-off-by: Maor Vanmak <maor@lightbitslabs.com>

---
+++ .config/nvim/init.lua
@@ -0,0 +1,40 @@
+--[[
+
+Neovim init file
+Version: 0.51.0 - 2022/03/25
+Maintainer: brainf+ck
+Website: https://github.com/brainfucksec/neovim-lua
+
+--]]
+
+-- Import Lua modules
+require('packer_init')
+require('core/settings')
+require('core/keymaps')
+require('core/statusline')
+--require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/vista')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
+require('plugins/nvim-spellsitter')
+require('plugins/lir')
+--
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<c-P>',
+    "<cmd>lua require('fzf-lua').files()<CR>",
+    { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<c-B>',
+    "<cmd>lua require('fzf-lua').buffers()<CR>",
+    { noremap = true, silent = true })
