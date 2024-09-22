2100dd0733fbcd97f96111b237fe18ab2125fb10
Maor Vanmak <maor.vanmak@gmail.com>
2024-09-22 16:08:19 +0300

    nvim-cmp: add support for integration with github copilot.
    Signed-off-by: Maor Vanmak <maor.vanmak@gmail.com>

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,98 +1,116 @@
 local M = {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"saadparwaiz1/cmp_luasnip",
-		"L3MON4D3/LuaSnip",
-	},
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "zbirenbaum/copilot.lua", -- Add Copilot.lua
+    "zbirenbaum/copilot-cmp", -- Add Copilot-cmp
+  },
 }
 
 M.config = function()
-	local cmp = require("cmp")
-	vim.opt.completeopt = { "menu", "menuone", "noselect" }
+  local cmp = require("cmp")
+  vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
-	local kind_icons = {
-		-- https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#basic-customisations
-		Text = " ",
-		Method = "󰆧",
-		Function = "ƒ ",
-		Constructor = " ",
-		Field = "󰜢 ",
-		Variable = " ",
-		Constant = " ",
-		Class = " ",
-		Interface = "󰜰 ",
-		Struct = " ",
-		Enum = "了 ",
-		EnumMember = " ",
-		Module = "",
-		Property = " ",
-		Unit = " ",
-		Value = "󰎠 ",
-		Keyword = "󰌆 ",
-		Snippet = " ",
-		File = " ",
-		Folder = " ",
-		Color = " ",
-	}
+  local kind_icons = {
+    Text = "T ",
+    Method = "M ",
+    Function = "ƒ ",
+    Constructor = " ",
+    Field = "f ",
+    Variable = "x ",
+    Constant = "c ",
+    Class = "C ",
+    Interface = "I ",
+    Struct = "S ",
+    Enum = "E ",
+    EnumMember = "e ",
+    Module = "M",
+    Property = " ",
+    Unit = "U ",
+    Value = "V ",
+    Keyword = "K ",
+    Snippet = " ",
+    File = " ",
+    Folder = " ",
+    Color = " ",
+    Copilot = "ai ",
+  }
 
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-			end,
-		},
-		window = {
-			-- completion = cmp.config.window.bordered(),
-			-- documentation = cmp.config.window.bordered(),
-		},
-		mapping = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		}),
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-			{ name = "nvim_lua" },
-			{ name = "luasnip" }, -- For luasnip users.
-		}, {
-			{ name = "buffer" },
-			{ name = "path" },
-		}, {
-			{ name = "neorg" },
-		}),
+  -- Setup copilot.lua
+  require("copilot").setup({
+    suggestion = { enabled = false }, -- Disable Copilot's inline suggestions
+    panel = { enabled = false },      -- Disable Copilot's panel
+    autostart = true,                 -- Enable autostart
+    filetypes = {
+      cpp = true,                     -- Enable Copilot for C++
+      go = true,                      -- Enable Copilot for Go
+      python = true,                  -- Enable Copilot for Python
+      ["*"] = false,                  -- Disable Copilot for all other file types
+    },
+  })
 
-		formatting = {
-			format = function(entry, vim_item)
-				-- Kind icons
-				vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-				-- Source
-				vim_item.menu = ({
-					buffer = "[Buffer]",
-					nvim_lsp = "[LSP]",
-					luasnip = "[LuaSnip]",
-					nvim_lua = "[NvimAPI]",
-					path = "[Path]",
-				})[entry.source.name]
-				return vim_item
-			end,
-		},
-	})
+  -- Setup copilot-cmp
+  require("copilot_cmp").setup()
 
-	cmp.setup.cmdline(":", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-			{ name = "path" },
-		}, {
-			{ name = "cmdline" },
-		}),
-	})
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "nvim_lua" },
+      { name = "luasnip" }, -- For luasnip users.
+      { name = "copilot" }, -- Add Copilot as a source
+    }, {
+      { name = "buffer" },
+      { name = "path" },
+    }),
+
+    formatting = {
+      format = function(entry, vim_item)
+        -- Kind icons
+        vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+        -- Source
+        vim_item.menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[NvimAPI]",
+          path = "[Path]",
+          copilot = "[Copilot]", -- Add Copilot to the source menu
+        })[entry.source.name]
+        return vim_item
+      end,
+    },
+  })
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      { name = "cmdline" },
+    }),
+  })
 end
 
 return M
