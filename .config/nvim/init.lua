a14b85b8cdb921f7d474d7a505a3b82c8d488250
Maor Vanmak <maor.vanmak@gmail.com>
2024-09-22 16:10:33 +0300

    remove keymaps file it's no longer needed
    Signed-off-by: Maor Vanmak <maor.vanmak@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,22 +10,22 @@ Website: https://github.com/brainfucksec/neovim-lua
 require("config.lazy")
 -- Import Lua modules
 require('core/settings')
-require('core/keymaps')
+-- require('core/keymaps')
 -- require('core/colors')
 -- require('core/statusline')
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 vim.api.nvim_set_keymap('n', '<c-P>',
-    "<cmd>lua require('fzf-lua').files()<CR>",
-    { noremap = true, silent = true })
+  "<cmd>lua require('fzf-lua').files()<CR>",
+  { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<c-B>',
-    "<cmd>lua require('fzf-lua').buffers()<CR>",
-    { noremap = true, silent = true })
+  "<cmd>lua require('fzf-lua').buffers()<CR>",
+  { noremap = true, silent = true })
